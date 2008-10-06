5b9029c3599cbe4c222984e70231f077bd0acc99
migueldiab <migueldiab@ff249e79-e855-0410-bb67-5be980414a48>
2008-10-06 00:54:24 +0000

    UiArticulo andando Agregar / Eliminar / Modificar / Listar

--- dda2008/src/uiConsola/UiArticulo.java
+++ dda2008/src/uiConsola/UiArticulo.java
@@ -24,7 +24,8 @@ public class UiArticulo
     costo = Consola.leerDouble(I18n.COSTO + " : ");
     //idMedida = Consola.menu(Fachada.listaMedidas());
     Articulo unArticulo = new Articulo(nombre, medida, cantidad, costo);
-    Fachada.agregarArticulo(unArticulo);
+    if (Fachada.agregarArticulo(unArticulo))
+      Consola.println(I18n.INGRESO_CORRECTO);
   }
 
   public static void borrarArticulo()
@@ -37,7 +38,7 @@ public class UiArticulo
    else {
      int posArticulo = Consola.menu(articulos);
      String confirma = Consola.leer(I18n.CONFIRMA_ELIMINAR);
-     if (confirma.equals(I18n.SI))
+     if (confirma.toUpperCase().equals(I18n.SI.toUpperCase()))
        Fachada.borrarArticulo((Articulo) articulos.get(posArticulo));
    }
   }
@@ -45,13 +46,41 @@ public class UiArticulo
   public static void modificarArticulo()
   {
     // TODO Auto-generated method stub
-    Fachada.modificarArticulo();
+    ArrayList articulos = Fachada.listadoArticulos();
+    if (articulos == null) {
+      Consola.println(I18n.LISTA_VACIA);
+    }
+    else {
+      int posArticulo = Consola.menu(articulos);
+      Articulo original = (Articulo) articulos.get(posArticulo);
+      String nombre = "";
+      int cantidad = 0;
+      double costo = 0;
+      int idMedida = 0;
+      Medida medida = new Medida();
+      nombre = Consola.modificar(I18n.NOMBRE, original.getNombre());
+      cantidad = Consola.modificarInt(I18n.CANTIDAD, original.getCantidad());
+      costo = Consola.modificarDouble(I18n.COSTO, original.getCosto());
+      //idMedida = Consola.menu(Fachada.listaMedidas());
+      String confirma = Consola.leer(I18n.CONFIRMA_MODIFICAR);
+      if (confirma.toUpperCase().equals(I18n.SI.toUpperCase()))
+      {
+        if (Fachada.modificarArticulo(
+             original,
+             new Articulo(nombre, medida, cantidad, costo))) {
+          Consola.println(I18n.MODIFICADO_OK);
+        }
+        else
+          Consola.println(I18n.ERROR);
+      }
+    }
+  
   }
 
   public static void listadoArticulos()
   {
     // TODO Auto-generated method stub
-   Fachada.listadoArticulos();
+   Consola.listado(Fachada.listadoArticulos());
    
   }
 
